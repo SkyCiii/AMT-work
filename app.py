2d74a934a7cbb1a2c674366adfce1379945e8ba8
Kevin <tingyi.chen0825@gmail.com>
2020-03-24 13:44:33 +0800

    repair update

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://trqtddsfwsydqm:d981464912f8a12a97e77298661800f75f904a02b66544aeca2ec587cb614722@ec2-54-197-48-79.compute-1.amazonaws.com:5432/d7ntbcs9nbv9tr'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
